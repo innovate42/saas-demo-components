8260d16a2114200bae3d7fb43677c4d0a44d1fb2
stevenletts <s.spencerletts@gmail.com>
2024-05-13 13:19:02 +0100

    feat: quik add item

--- components/quick-add-on/index.js
+++ components/quick-add-on/index.js
@@ -1,28 +1,50 @@
-//@flow
 import * as React from "react";
-import {useCampaign} from "@limio/sdk"
-type Props = {
+import {useBasket, useCampaign} from "@limio/sdk";
+import {useDispatch} from "@limio/shop-redux"
+import {addToBasketAction} from "@limio/shop-redux/src/shop/redux"
+
+import "./index.css";
+
+type Props = {};
+
+function QuickAddOn({includeDesc, ctaText}: Props): React.Node {
+    const {addOns = []} = useCampaign();
+    const {basketItems = {}} = useBasket();
+    const dispatch = useDispatch()
+
+    const {addOns: basketAddOns} = basketItems;
+
+    const addOnInBasket = (addOn) => basketAddOns && basketAddOns.find(basketAddOn => basketAddOn.id === addOn.id);
+
+    const addToBasket = (addOn) => {
+        if (addOnInBasket(addOn)) {
+            return;
+        }
+
+        const newBasketAddOns = basketAddOns ? [...basketAddOns, addOn] : [addOn];
+        const {offer, quantity} = basketItems;
+
+        dispatch(addToBasketAction({offer: offer, addOns: newBasketAddOns, quantity: quantity, pushToCheckout: false}))
+    };
+
+    return (
+        <div className="quick-add-container">
+            {addOns.map(addOn => (
+                <div className="quick-add-item" key={addOn.id}>
+                    <div className="quick-add-item-content">
+                        <div className="quick-add-item-name">{addOn.name}</div>
+                        <div className="quick-add-item-price">{addOn.price}</div>
+
+                        {includeDesc &&
+                            <div dangerouslySetInnerHTML={{__html: addOn.data.attributes.description__limio}}></div>}
+                        <button className="quick-add-to-basket" onClick={() => addToBasket(addOn)}>
+                            {ctaText || "Add to basket"}
+                        </button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
-function QuickAddOn({ }: Props): React.Node {
-    const {addOns =[]} = useCampaign();
-
-    console.log(addOns)
-
-  return (
-    <div className="quick-add-container">
-      {addOns.map(addOn => {
-        return (
-          <div className="quick-add-item" key={addOn.id}>
-            <div className="quick-add-item-content">
-              <div className="quick-add-item-name">{addOn.name}</div>
-              <div className="quick-add-item-price">{addOn.price}</div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
-
-export default QuickAddOn
+export default QuickAddOn;
