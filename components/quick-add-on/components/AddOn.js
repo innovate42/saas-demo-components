9ea34e2543b62a8fe41804a7a3a94f057b3e6927
stevenletts <s.spencerletts@gmail.com>
2024-05-13 15:09:39 +0100

    fix imports

--- components/quick-add-on/components/AddOn.js
+++ components/quick-add-on/components/AddOn.js
@@ -4,13 +4,11 @@ import * as R from "ramda";
 import {useBasket} from "@limio/sdk";
 import {useDispatch} from "@limio/shop-redux"
 import {addToBasketAction} from "@limio/shop-redux/src/shop/redux"
-import {formatDisplayPrice, sanitizeString} from "@limio/utils/string";
-
+import {sanitizeString} from "@limio/shop/src/shop/offers/helpers"
 
 const AddOn = ({addOn}) => {
     const {
         display_name__limio,
-        display_price__limio,
         description__limio,
         price__limio,
     } = addOn.data.attributes;
@@ -35,15 +33,21 @@ const AddOn = ({addOn}) => {
         dispatch(addToBasketAction({offer: offer, addOns: newBasketAddOns, quantity: quantity, pushToCheckout: false}))
     };
 
+    const formatCurrency = (amount = 0, currency = "GBP") => {
+        if (typeof Intl !== "undefined" && typeof Intl.NumberFormat !== "undefined") {
+            return new Intl.NumberFormat("en-US", {style: "currency", currency: currency}).format(amount)
+        } else {
+            return `${currency} ${amount}`
+        }
+    }
+
+
     return (
         <div className="add-on">
             <h3 className="add-on-title">{display_name__limio}</h3>
             <div className="price-container">
                 <span className="price" dangerouslySetInnerHTML={{
-                    __html: sanitizeString(formatDisplayPrice(display_price__limio, [{
-                        currencyCode: price__limio[0].currencyCode,
-                        value: price__limio[0].value,
-                    }]))
+                    __html: sanitizeString(formatCurrency(price__limio[0].value, price__limio[0].currencyCode))
                 }}></span>
             </div>
             <p className="subtext"></p>
