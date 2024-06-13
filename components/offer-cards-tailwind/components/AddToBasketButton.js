354d51bc82ccfb64695872ad36882c7a6360c135
timJFrame <74713500+timJFrame@users.noreply.github.com>
2024-06-13 12:21:00 +0100

    change button to div

--- components/offer-cards-tailwind/components/AddToBasketButton.js
+++ components/offer-cards-tailwind/components/AddToBasketButton.js
@@ -8,25 +8,25 @@ export function AddToBasketButton({ offer, primaryColor }): React.Node {
   const offerInBasket = basketItems?.find(basketItem => basketItem.offer?.id === offer.id)
 
   return (
-      <>
-          {
-            !offerInBasket
-                ? <button type="button"
-                          onClick={() => addToBasket(offer)}
-                          className="mt-auto text-white  hover:bg-white f font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 add-to-basket-button"
-                          style={{backgroundColor: `${primaryColor}`}}
-                >
-                  {cta_text__limio}
-                </button>
-                : <button type="button"
-                          onClick={() => removeFromBasket(offer)}
-                          className="mt-auto  text-white  hover:text-white border   font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 add-to-basket-button"
-                          style={{backgroundColor: `${primaryColor}`}}
-                >
-                  {"Remove"}
-                </button>
-          }
-      </>
-    )
+    <>
+        {
+          !offerInBasket
+              ? <div 
+                        onClick={() => addToBasket(offer)}
+                        className="mt-auto text-white  hover:bg-white f font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 add-to-basket-button"
+                        style={{backgroundColor: `${primaryColor}`}}
+              >
+                {cta_text__limio}
+              </div>
+              : <div 
+                        onClick={() => removeFromBasket(offer)}
+                        className="mt-auto  text-white  hover:text-white border   font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 add-to-basket-button"
+                        style={{backgroundColor: `${primaryColor}`}}
+              >
+                {"Remove"}
+              </div>
+        }
+    </>
+  )
 
 }
