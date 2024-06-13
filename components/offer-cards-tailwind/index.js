b2b129805dd90945759fafe7a2c1308d13178059
timJFrame <74713500+timJFrame@users.noreply.github.com>
2024-06-13 17:05:39 +0100

    fix error

--- components/offer-cards-tailwind/index.js
+++ components/offer-cards-tailwind/index.js
@@ -92,7 +92,7 @@ export const OfferCards = ({heading, subheading, showImage, componentId, offerWi
               ))}
           </div>
           <div className=" flex justify-center flex-wrap ">
-          {selectedGroupOffers ? (
+          {selectedGroup.length > 0 ? (
                 selectedGroupOffers.map((offer, i) => (
                     <Offer key={`${offer.path}/parent-${i}`} offer={offer} showImage={showImage} offerWidth={offerWidth} primaryColor={primaryColor__limio_color}/>
                 ))
