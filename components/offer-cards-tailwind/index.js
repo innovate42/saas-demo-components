a93b8ad9ede4055ec42cceb97cc204d4e62ed016
timJFrame <74713500+timJFrame@users.noreply.github.com>
2024-06-14 13:10:28 +0100

    offer card updates

--- components/offer-cards-tailwind/index.js
+++ components/offer-cards-tailwind/index.js
@@ -19,15 +19,31 @@ function groupOffers(
   offers,
   groupLabels
 ) {
+
   // Take current offers and split them array groups
   const groups = R.groupBy(
       R.path(["data", "attributes", "group__limio"]),
       offers
   );
 
+  // The order of the groups object is not guaranteed, so we need to sort it to be the same order as the groupLabels
+  const groupLabelArray = groupLabels.map((group) => group.id)
+  function reorderKeys(obj, order) {
+    const newObj = {};
+    order.forEach(key => {
+        if (obj.hasOwnProperty(key)) {
+            newObj[key] = obj[key];
+        }
+    });
+    return newObj;
+}
+
+// Reorder the object keys
+const sortedGroup = reorderKeys(groups, groupLabelArray);
 
 
-  const groupedOffers = Object.keys(groups).map((groupId) => {
+
+  const groupedOffers = Object.keys(sortedGroup).map((groupId) => {
 
     // return just the groups that are defined on offer and component
       const group = groupLabels.find((group) => group.id === groupId) 
@@ -67,25 +83,24 @@ export const OfferCards = ({heading, subheading, showImage, componentId, offerWi
   }, [offerGroups])
 
 
-
   useEffect(() => {
     typeof performance !== "undefined" && performance?.mark?.("offers-init");
   }, []);
 
   return (
       <section className="bg-white dark:bg-gray-900" id={componentId}>
-        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 flex flex-col">
           <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{heading}</h2>
             <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">{subheading}</p>
           </div>
          {showGroupedOffers ?
           <>
-          <div className="flex p-2 mx-auto max-w-xs text-center text-gray-900 bg-gray-100 rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-4 dark:bg-gray-800 dark:text-white mb-8">
+          <div className="flex p-2 mx-auto  text-center text-gray-900 bg-gray-100 rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-4 dark:bg-gray-800 dark:text-white mb-8"
+          style={{minWidth: "320px"}}
+          >
               {offerGroups.map((offerGroup, i) => (
-                  <button onClick={() => setSelectedGroup(offerGroup.id)}   key={`${offerGroup.id}-${i}`} className={`whitespace-nowrap py-2.5 px-1.5 sm:px-3.5 mx-auto w-1/2  dark:bg-gray-600 hover:bg-gray-500 dark:text-white hover:text-white rounded-md text-xs sm:text-lg  ${selectedGroup === offerGroup.id ? "dark:bg-gray-400  dark:text-white bg-white" : ""}`}
-         
-               
+                  <button onClick={() => setSelectedGroup(offerGroup.id)}   key={`${offerGroup.id}-${i}`} className={`whitespace-nowrap py-2.5 px-1.5 sm:px-3.5 mx-auto   dark:bg-gray-600 hover:bg-gray-500 dark:text-white hover:text-white rounded-md text-xs sm:text-lg  ${selectedGroup === offerGroup.id ? "dark:bg-gray-400  dark:text-white bg-white" : ""}`}
                   >
                       {offerGroup.label}
                   </button>
@@ -97,7 +112,7 @@ export const OfferCards = ({heading, subheading, showImage, componentId, offerWi
                     <Offer key={`${offer.path}/parent-${i}`} offer={offer} showImage={showImage} offerWidth={offerWidth} primaryColor={primaryColor__limio_color}/>
                 ))
             ) : (
-                <p>No offers to display...Please add a label to view offers</p>
+                <p>No offers to display...Please an a label to view offers</p>
             )}
           </div>
           </>
