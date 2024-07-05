552799819eb198480dc76ca789d2e338925b1aaf
stevenletts <s.spencerletts@gmail.com>
2024-07-05 11:11:46 +0100

    fix: max width on offer card

--- components/offer-cards-upkeep/index.js
+++ components/offer-cards-upkeep/index.js
@@ -107,8 +107,8 @@ export const OfferCardsUpkeep= ({
 
     return (
         <section className="bg-white dark:bg-gray-900" id={componentId}>
-            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 flex flex-col">
-                <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
+            <div className="py-8 px-4 mx-auto  lg:py-16 lg:px-6 flex flex-col">
+                <div className="mx-auto  text-center mb-8 lg:mb-12">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{heading}</h2>
                     <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">{subheading}</p>
                 </div>
