d1cc7c3b7f5cf457afd6ceee73c5536e839710d9
stevenletts <s.spencerletts@gmail.com>
2024-07-05 16:25:17 +0100

    clean text in sectuin component

--- components/section-tailwind/index.js
+++ components/section-tailwind/index.js
@@ -24,12 +24,16 @@ export default function Section(props: Props): React.Node {
     componentId,
   } = props;
 
+  const cleanText= (text) => {
+    return text.replace(/(<([^>]+)>)/gi, "")
+  }
+
   return (
       <section className="bg-white dark:bg-gray-900" id={componentId}>
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">{header}</h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">{text}</p>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">{cleanText(text)}</p>
             {
               (showButtons && buttons.length > 0) && buttons.map((button, i) => (
                   <a href={button.buttonLocation}
