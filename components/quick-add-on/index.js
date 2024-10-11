8c9df31055348098bc6d69e084bdbbb0acec2f33
stevenletts <s.spencerletts@gmail.com>
2024-10-11 12:38:25 +0100

    quick add on:

--- components/quick-add-on/index.js
+++ components/quick-add-on/index.js
@@ -1,38 +1,46 @@
 // @flow
-import React from "react";
-import {useCampaign} from "@limio/sdk";
-import AddOn from "./components/AddOn";
-import "./index.css";
+import React from "react"
+import { useCampaign } from "@limio/sdk"
+import AddOn from "./components/AddOn"
+import "./index.css"
 
 type Props = {
-    heading: string,
-    subheading: string
-};
+  heading: string,
+  subheading: string
+}
 
-export const AddOnCards = ({heading, subheading, ctaText}: Props) => {
-    const {addOns = []} = useCampaign()
+export const AddOnCards = ({ heading, subheading, ctaText }: Props) => {
+  const { addOns = [] } = useCampaign()
+  const { basketItems = [] } = useBasket()
+  const currentOffer = basketItems[0]?.offer
 
-    return (
-        <section className="add-on-section">
-            <div className="add-on-container">
-                {heading || subheading ?
-                    <div className="add-on-heading-container">
-                        {heading && <h2 className="add-on-heading">Heading Text</h2>}
-                        {subheading && <p className="add-on-subheading">Subheading Text</p>}
-                    </div> : <> </>}
-                <div className="add-on-grid">
-                    {addOns.length > 0 ? (
-                        addOns.map((addOn) => (
-                            <AddOn key={addOn.path + "/parent"} addOn={addOn} ctaText={ctaText}
-                            />
-                        ))
-                    ) : (
-                        <p>No add-ons to display...</p>
-                    )}
-                </div>
-            </div>
-        </section>
-    );
-};
+  const filteredAddOns = addOns.filter(addOn => {
+    const { data } = addOn
+    const { compatibleLabel } = data.attributes
+    return compatibleLabel.some(label => currentOffer.data.attributes.compatibleLabel.includes(label))
+  })
 
-export default AddOnCards;
+  return (
+    <section className="add-on-section">
+      <div className="add-on-container">
+        {heading || subheading ? (
+          <div className="add-on-heading-container">
+            {heading && <h2 className="add-on-heading">Heading Text</h2>}
+            {subheading && <p className="add-on-subheading">Subheading Text</p>}
+          </div>
+        ) : (
+          <> </>
+        )}
+        <div className="add-on-grid">
+          {filteredAddOns.length > 0 ? (
+            filteredAddOns.map(addOn => <AddOn key={addOn.path + "/parent"} addOn={addOn} ctaText={ctaText} />)
+          ) : (
+            <p>No add-ons to display...</p>
+          )}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AddOnCards
