2e3d494aeaaa14b7b9269d65042c00cfad2f6e14
stevenletts <s.spencerletts@gmail.com>
2024-10-17 11:10:56 +0100

    chore:copy init checkout btn

---
+++ components/init-checkout-button-saas-demo/index.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Button } from "@limio/component-library"
+import { useBasket } from "@limio/sdk"
+
+function InitiateCheckoutButton({ buttonLabel }): JSX.Element {
+  const { basketItems, goToCheckout } = useBasket()
+
+  const buttonIsDisabled = !basketItems || basketItems.length === 0
+
+  const handleGoToCheckout = () => {
+    if (buttonIsDisabled) return
+
+    const checkout = basketItems[0].offer?.data?.attributes?.checkout__limio
+
+    const options = {
+      journey: { checkout }
+    }
+
+    goToCheckout(null, options)
+  }
+
+  return (
+    <Button onClick={handleGoToCheckout} disabled={buttonIsDisabled} className="tw-w-full tw-my-1" variant="primary">
+      {buttonLabel}
+    </Button>
+  )
+}
+
+export default InitiateCheckoutButton
